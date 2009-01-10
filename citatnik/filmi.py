87e25146200738799e5522b8b5141ad572630690
aquilax <aquilax@5baab640-de13-11dd-a8aa-93fc2732f0ee>
2009-01-10 11:01:45 +0000

    Added Movies (Extended classes)

--- citatnik/filmi.py
+++ citatnik/filmi.py
@@ -7,30 +7,42 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 
+class Movie(db.Model):
+	title = db.StringProperty()
+	year = db.StringProperty()
+	description = db.StringProperty(multiline=True)
+	imdb = db.StringProperty()
+	visible = db.BooleanProperty(default=False)
+
 class Quote(db.Model):
-  movie = db.StringProperty()
-  quote = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
-  author = db.UserProperty()
-  rating = db.RatingProperty(default=0)
-  visible = db.BooleanProperty(default=False)
+	movie = db.ReferenceProperty(Movie)
+	quote = db.StringProperty(multiline=True)
+	author = db.UserProperty()
+	rating = db.RatingProperty(default=0)
+	visible = db.BooleanProperty(default=False)
+	date = db.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp.RequestHandler):
   def get(self):
     quotes = db.GqlQuery("SELECT * FROM Quote ORDER BY date DESC")
     template_values = {
-        'quotes': quotes
-        }
+			'quotes': quotes
+      }
     path = os.path.join(os.path.dirname(__file__), 'index.html')
     self.response.out.write(template.render(path, template_values))
 
 class AddQuote(webapp.RequestHandler):
   def get(self):
-    template_values = {}
-    path = os.path.join(os.path.dirname(__file__), 'add.html')
-    self.response.out.write(template.render(path, template_values))
+		movies = db.GqlQuery("SELECT * FROM Movie ORDER BY title")
+		template_values = {
+			'movies': movies
+		}
+		path = os.path.join(os.path.dirname(__file__), 'add.html')
+		self.response.out.write(template.render(path, template_values))
 
   def post(self):
+		if len(self.request.get('movie')) = 0:
+			movie = Movie()
     quote = Quote()
     if users.get_current_user():
       quote.user = users.get_current_user()
